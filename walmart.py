6bf8e671534d6496128a31bdcf71836eb44c9379
Theriley106 <christopherlambert106@gmail.com>
2018-11-20 11:21:08 -0500

    Updated walmart

--- walmart.py
+++ walmart.py
@@ -2,13 +2,20 @@ import requests
 import json
 import headers
 import time
-VERBOSE = False
+import csv
+VERBOSE = 0
 REQUEST_TIMEOUT = 10
 NETWORK_RETRY = 3
 PAUSE_BETWEEN_REQUESTS = 0
 # This is the short pause between consequetive network requests
 
 TERRAFIRM_URL = "https://www.walmart.com/terra-firma/fetch"
+
+class Hasher(dict):
+    # https://stackoverflow.com/a/3405143/190597
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
 
 def network_request(url, headers, post=False, params=None, timeout=None, data=None, network_retry=None):
 	# This is the function that makes network requests
@@ -29,18 +36,44 @@ def network_request(url, headers, post=False, params=None, timeout=None, data=No
 				# This means it was successful
 				return res
 		time.sleep(PAUSE_BETWEEN_REQUESTS)
+		# Pause to prevent back to back requests
 
 
 def returnPricing(terrafirmaDoc):
 	# Extracts pricing information from the terrafirm API response
+	terrafirmaDoc = Hasher(terrafirmaDoc)
 	for key, value in terrafirmaDoc['payload']['offers'].items():
 		try:
 			price = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceMap']['CURRENT']['price']
 			store = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeId']
 			quantity = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]["inStoreStockStatus"]
-			return {"Store": store, "Price": price, "Quantity":quantity}
+			rollback = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceDisplayCodes']['rollback']
+			strikethrough = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceDisplayCodes']['strikethrough']
+			reducedPrice = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceDisplayCodes']['reducedPrice']
+			clearance = terrafirmaDoc['payload']['offers'][key]['pricesInfo']['priceDisplayCodes']['clearance']
+			storeCity = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeCity']
+			storeName = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeName']
+			storeAddress = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeAddress']
+			storeStateOrProvinceCode = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storeStateOrProvinceCode']
+			storePostalCode = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['storePostalCode']
+			availability = terrafirmaDoc['payload']['offers'][key]['fulfillment']['pickupOptions'][0]['availability']
+			productInfo = terrafirmaDoc['payload']['products']
+			productInfo = productInfo[productInfo.keys()[0]]
+			productInfo = Hasher(productInfo)
+			primaryProductId = productInfo['primaryProductId']
+			wupc = productInfo['wupc']
+			usItemId = productInfo['usItemId']
+			upc = productInfo['upc']
+			productType = productInfo['productType']
+			longSku = productInfo['productAttributes']['sku']
+			category = productInfo['productAttributes']['productCategory']['categoryPath']
+			information = {"price: ": price, "rollback: ": rollback, "strikethrough: ": strikethrough, "reducedPrice: ": reducedPrice, "clearance: ": clearance, "store: ": store, "storeCity: ": storeCity, "storeName: ": storeName, "storeAddress: ": storeAddress, "storeStateOrProvinceCode: ": storeStateOrProvinceCode, "storePostalCode: ": storePostalCode, "availability: ": availability, "quantity: ": quantity, "primaryProductId: ": primaryProductId, "wupc: ": wupc, "usItemId: ": usItemId, "upc: ": upc, "productType: ": productType, "longSku: ": longSku, "category: ": category}
+			for key, val in information.items():
+				if "Hasher" in str(type(information[key])):
+					information[key] = ""
+			return information
 		except Exception as exp:
-			if VERBOSE:
+			if VERBOSE > 1:
 				print exp
 			pass
 
@@ -52,9 +85,10 @@ def local_item_info(store, sku):
 	# Parameters that specify the data we want to return
 	data = '{{"itemId":"{}","paginationContext":{{"selected":false}},"storeFrontIds":[{{"usStoreId":{},"preferred":false,"semStore":false}}]}}'.format(sku, store)
 	response = network_request(TERRAFIRM_URL, post=True, headers=header, params=params, data=data)
-	print response.status_code
-	if VERBOSE:
-		print json.dumps(response.json())
+	# This calls the API endpoint
+	if VERBOSE > 3:
+		# This will print the api response for debugging
+		print response.json()
 	return returnPricing(response.json())
 
 def convertSKUToUPC(sku):
@@ -67,9 +101,20 @@ def convertSKUToUPC(sku):
 		print("Converted SKU: {} to UPC: {}".format(sku, upc))
 		return upc
 	except:
-		if VERBOSE:
+		if VERBOSE > 1:
 			print("Converting to UPC failed.")
 		return None
 
+def GrabAllStoreNumbers():
+	ListOfStores = []
+	with open('Walmarts.csv', 'r') as f:
+		reader = csv.reader(f)
+		your_list = list(reader)
+	for line in your_list:
+		if 'Walmart Supercenter' in str(line[1]):
+			ListOfStores.append(line[0])
+	return ListOfStores
+
 if __name__ == '__main__':
-	print local_item_info('2265', '779112907')
+	VERBOSE = 3
+	print local_item_info('2265', '435188866')
